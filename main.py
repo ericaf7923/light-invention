bf0f0119d9f8f3a0069faf9c4e2b90f31f5bad8c
ericaf7923 <72775447+ericaf7923@users.noreply.github.com>
2021-01-06 21:55:13 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
+light.set_brightness(300)
 while True: 
 
-    #if light level is greater than 4, then set neopixels off
+    #if light level is greater than 5, then set neopixels off
     print("light level: " + input.light_level())
-    if input.light_level() > 4:
+    if input.light_level() > 5 :
         light.clear()
 
-    #if light level is equal to or greater than 2, then set neopixels pink 
+    #if light level is equal to or greater than 2, then set neopixels pink
     elif input.light_level() >= 2 :
         light.set_all(color.rgb(255,56,152))
 
-    else: 
+    else:
         light.set_all(color.rgb(255,56,152))
+    
--- main.ts
+++ main.ts
@@ -1,10 +1,11 @@
+light.setBrightness(300)
 while (true) {
-    // if light level is greater than 4, then set neopixels off
+    // if light level is greater than 5, then set neopixels off
     console.log("light level: " + input.lightLevel())
-    if (input.lightLevel() > 4) {
+    if (input.lightLevel() > 5) {
         light.clear()
     } else if (input.lightLevel() >= 2) {
-        // if light level is equal to or greater than 2, then set neopixels pink 
+        // if light level is equal to or greater than 2, then set neopixels pink
         light.setAll(color.rgb(255, 56, 152))
     } else {
         light.setAll(color.rgb(255, 56, 152))
